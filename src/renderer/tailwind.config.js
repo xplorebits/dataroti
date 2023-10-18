0f78a6683bd9e84897b50242f8f44a501538b220
saikksub <saikksub@gmail.com>
2023-10-18 22:59:57 +0530

    Adds tailwindcss

---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}', './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
