f882ad29a7214dc5da3c655fe151c02e71ed576b
saikksub <saikksub@gmail.com>
2023-10-18 23:10:18 +0530

    Adds tailwindcss

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <div class="bg-black text-white">hello 2</div>
+  <div class="realtive h-screen">
+    <div class="absolute top-0 bottom-0 left-0 w-64 bg-white border-r border-nuetral-100">
+      <div class="flex flex-col gap-3 p-3">
+        <p class="text-sm font-medium">Dataroti</p>
+
+        <button
+          class="bg-neutral-50 border border-neutral-100 text-sm hover:bg-white rounded-md w-full px-3 py-2"
+        >
+          Add Connection
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
